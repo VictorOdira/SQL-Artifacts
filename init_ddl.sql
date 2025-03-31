e80fa220d855843b9fdda0b58ab8bffcd284f7f0
Odingo Odira <118199038+VictorOdira@users.noreply.github.com>
2025-03-31 11:18:02 +0300

    Update init_ddl.sql

--- init_ddl.sql
+++ init_ddl.sql
@@ -1,10 +1,6 @@
--- 1. Creating a new database called Human_resource 
-CREATE DATABASE Human_resource;
+-- DATA DEFINITION LANGUAGE (DDL)
 
--- 2. Making the database active so that I can create a table within. 
-USE Human_resource;
-
--- 3. Creating a table called BioData in the database using MySQL 
+-- 1. Creating a table called BioData in the database using MySQL 
 CREATE TABLE BioData (
     EmployeeID INT PRIMARY KEY,
     FirstName VARCHAR(50) NOT NULL,
@@ -15,7 +11,8 @@ CREATE TABLE BioData (
     EmailAddress VARCHAR(100) UNIQUE,
     Residence VARCHAR(255)
 );
--- 4. Creating a table called JobDetails in the database using MySQL
+
+-- 2. Creating a table called JobDetails in the database using MySQL
 
 CREATE TABLE JobDetails (
     JobDetailID INT PRIMARY KEY,
@@ -28,7 +25,7 @@ CREATE TABLE JobDetails (
     FOREIGN KEY (SupervisorID) REFERENCES BioData(EmployeeID) -- Supervisor is also an employee
 );
 
--- 5. Creating a table called RenumerationAndAllowanceDetails in the database using MySQL
+-- 3. Creating a table called RenumerationAndAllowanceDetails in the database using MySQL
 CREATE TABLE RenumerationAndAllowanceDetails (
     RenumerationAllowanceID INT PRIMARY KEY,
     EmployeeID INT,
@@ -43,7 +40,7 @@ CREATE TABLE RenumerationAndAllowanceDetails (
     FOREIGN KEY (EmployeeID) REFERENCES BioData(EmployeeID)
 );
 
--- 6. Creating a table called PerformanceDetails in the database using MySQL
+-- 4. Creating a table called PerformanceDetails in the database using MySQL
 
 CREATE TABLE PerformanceDetails (
     PerformanceID INT PRIMARY KEY,
@@ -54,16 +51,16 @@ CREATE TABLE PerformanceDetails (
     FOREIGN KEY (EmployeeID) REFERENCES BioData(EmployeeID)
 );
 
--- 6. Adding a new column called  MidTermReviewStatus in the table - PerformanceDetails in the database using MySQL after  EmployeeID column
+-- 5. Adding a new column called  MidTermReviewStatus in the table - PerformanceDetails in the database using MySQL after  EmployeeID column
 
 ALTER TABLE PerformanceDetails
 ADD COLUMN MidTermReviewStatus VARCHAR(50) NOT NULL CHECK (MidTermReviewStatus IN ("Reviewed",  "Not Reviewed")) AFTER EmployeeID;
 
--- 7. Deleting a column called  MidTermReviewStatus from a table - PerformanceDetails in the database using MySQL.
+-- 6. Deleting a column called  MidTermReviewStatus from a table - PerformanceDetails in the database using MySQL.
 
 ALTER TABLE PerformanceDetails
 DROP COLUMN MidTermReviewStatus;
 
--- 8. Deleting an entire table called PerformanceDetails
+-- 7. Deleting an entire table called PerformanceDetails
 
 DROP TABLE PerformanceDetails;
