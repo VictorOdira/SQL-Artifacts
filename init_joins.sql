09b056d83cc968d8e331a55d2af4ca04bb4849af
Odingo Odira <118199038+VictorOdira@users.noreply.github.com>
2025-04-01 12:14:12 +0300

    Create init_joins.sql

---
+++ init_joins.sql
@@ -0,0 +1,58 @@
+
+-- SQL JOINS 
+
+-- INNER JOIN 
+
+SELECT 
+	B.EmployeeID,
+	B.FirstName,
+  B.LastName AS 'Surname',
+  B.Gender,
+  P.MidTermReviewStatus As 'Status',
+  P.PerformanceRating AS Rating,
+  P.ReviewComments AS 'Comments'
+FROM BioData AS B
+INNER JOIN PerformanceDetails AS P
+	     ON P.EmployeeID = B.EmployeeID;
+
+--- LEFT JOIN (or LEFT OUTER JOIN)
+
+SELECT 
+	B.EmployeeID,
+	B.FirstName,
+    B.LastName AS 'Surname',
+    B.Gender,
+    P.MidTermReviewStatus As 'Status',
+    P.PerformanceRating AS Rating,
+    P.ReviewComments AS 'Comments'
+FROM BioData AS B
+LEFT JOIN PerformanceDetails AS P
+	     ON P.EmployeeID = B.EmployeeID;
+
+-- RIGHT JOIN (or RIGHT OUTER JOIN)
+
+SELECT 
+	B.EmployeeID,
+	B.FirstName,
+  B.LastName AS 'Surname',
+  B.Gender,
+  P.MidTermReviewStatus As 'Status',
+  P.PerformanceRating AS Rating,
+  P.ReviewComments AS 'Comments'
+FROM BioData AS B
+RIGHT JOIN PerformanceDetails AS P
+	     ON P.EmployeeID = B.EmployeeID;
+
+--  FULL JOIN (or FULL OUTER JOIN)
+
+SELECT 
+	B.EmployeeID,
+	B.FirstName,
+  B.LastName AS 'Surname',
+  B.Gender,
+  P.MidTermReviewStatus As 'Status',
+  P.PerformanceRating AS Rating,
+  P.ReviewComments AS 'Comments'
+FROM BioData AS B
+FULL JOIN PerformanceDetails AS P
+	     ON P.EmployeeID = B.EmployeeID;
